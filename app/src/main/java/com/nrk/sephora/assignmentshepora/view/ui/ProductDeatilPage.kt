59061a115355e6c3fcff1460ea0ab9762f4d7ec4
nirock <nirock2006@gmail.com>
2021-11-02 16:17:08 +0800

    updated Product details page with changing images

--- app/src/main/java/com/nrk/sephora/assignmentshepora/view/ui/ProductDeatilPage.kt
+++ app/src/main/java/com/nrk/sephora/assignmentshepora/view/ui/ProductDeatilPage.kt
@@ -4,10 +4,15 @@ import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
@@ -21,6 +26,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.constraintlayout.compose.ConstraintLayout
+import androidx.core.text.parseAsHtml
 import androidx.hilt.navigation.compose.hiltViewModel
 import coil.compose.rememberImagePainter
 import com.nrk.sephora.assignmentshepora.R
@@ -86,12 +92,14 @@ fun ProductView(product: ProductModel) {
             .fillMaxHeight()
             .verticalScroll(rememberScrollState())
     ) {
-        val (headerImage, imageRow, title,subTitle,ratingRow,priceRow,descriptionTitle) = createRefs()
+        val selectedImage  = rememberSaveable { mutableStateOf(product.attributes.featured_image_urls.firstOrNull()?:"") }
+        val selectedImageIndex  = rememberSaveable { mutableStateOf(0) }
+        val (headerImage, imageRow, title,subTitle,ratingRow,priceRow,descriptionArea,howToArea,whatItDoes) = createRefs()
         Image(
             contentScale = ContentScale.Crop,
             painter =
             rememberImagePainter(
-                data = product.attributes.featured_image_urls.firstOrNull(),
+                data = selectedImage.value,
                 builder = {
                     placeholder(R.drawable.ic_launcher_background)
                     crossfade(true)
@@ -110,7 +118,7 @@ fun ProductView(product: ProductModel) {
         )
 
         LazyRow(content = {
-            items(product.attributes.featured_image_urls) { imageUrl ->
+            itemsIndexed(product.attributes.featured_image_urls) { index, imageUrl ->
                 Image(
                     contentScale = ContentScale.Crop,
                     painter =
@@ -127,7 +135,16 @@ fun ProductView(product: ProductModel) {
                         .absolutePadding(top = 8.dp, left = 8.dp, right = 8.dp)
                         .height(50.dp)
                         .width(50.dp)
-                        .border(2.dp, Color(0xFFDB7741), RectangleShape)
+                        .border(
+                            width = 2.dp,
+                            color = if(index == selectedImageIndex.value) Color(0xFF3093DF) else Color(
+                                0xFF656666
+                            ),
+                            shape = RectangleShape)
+                        .clickable {
+                            selectedImageIndex.value = index
+                            selectedImage.value = imageUrl
+                        }
                 )
             }
         },
@@ -235,20 +252,82 @@ fun ProductView(product: ProductModel) {
             )
         }
 
-        Text(
-            text = "Description",
-            color = Color.Black,
-            maxLines = 1,
-            overflow = TextOverflow.Ellipsis,
-            fontWeight = FontWeight.Bold,
-            fontSize = 16.sp ,
+        composeParagrapthWithTitle(
+            title = "DESCRIPTION",
+            body = product.attributes.description,
             modifier = Modifier
-                .constrainAs(descriptionTitle) {
+                .constrainAs(descriptionArea) {
                     top.linkTo(priceRow.bottom)
-                    start.linkTo(headerImage.start)
+                    start.linkTo(parent.start)
+                    start.linkTo(parent.start)
                 }
+                .fillMaxWidth()
                 .wrapContentHeight()
-                .absolutePadding(bottom = 8.dp, left = 12.dp, right = 12.dp)
+                .absolutePadding(bottom = 8.dp, left = 6.dp, right = 6.dp)
+        )
+        composeParagrapthWithTitle(
+            title = "How to use:",
+            body = product.attributes.description,
+            modifier = Modifier
+                .constrainAs(howToArea) {
+                    top.linkTo(descriptionArea.bottom)
+                    start.linkTo(parent.start)
+                    start.linkTo(parent.start)
+                }
+                .fillMaxWidth()
+                .wrapContentHeight()
+                .absolutePadding(bottom = 8.dp, left = 6.dp, right = 6.dp)
+        )
+        composeParagrapthWithTitle(
+            title = "What It Does:",
+            body = product.attributes.how_to_text,
+            modifier = Modifier
+                .constrainAs(whatItDoes) {
+                    top.linkTo(howToArea.bottom)
+                    start.linkTo(parent.start)
+                    start.linkTo(parent.start)
+                }
+                .fillMaxWidth()
+                .wrapContentHeight()
+                .absolutePadding(bottom = 8.dp, left = 6.dp, right = 6.dp)
         )
     }
+
+}
+
+
+@Composable
+fun composeParagrapthWithTitle(title:String,body:String,modifier:Modifier){
+    Column(modifier = modifier,
+        verticalArrangement = Arrangement.Top,
+        horizontalAlignment = Alignment.Start) {
+        if(body.isNullOrEmpty().not()){
+            Text(
+                text = title,
+                color = Color.Black,
+                maxLines = 1,
+                overflow = TextOverflow.Ellipsis,
+                fontWeight = FontWeight.Bold,
+                fontSize = 16.sp ,
+                textAlign = TextAlign.Left,
+                modifier = modifier
+                    .fillMaxWidth()
+                    .wrapContentHeight()
+
+            )
+            Text(
+                text = body,
+                color = Color.Black,
+                fontWeight = FontWeight.Normal,
+                fontSize = 14.sp ,
+                modifier = modifier
+                    .fillMaxWidth()
+                    .wrapContentHeight()
+            )
+        }else{
+            Text("")
+        }
+
+    }
+
 }
